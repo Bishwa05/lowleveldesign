757fe57024584db47c9ddf0a2b5d3e18e9fb7ae2
Bishwa <bishwa.nayak@sap.com>
2021-11-16 23:17:31 +0530

    classes

--- src/snakegame/Snake.java
+++ src/snakegame/Snake.java
@@ -21,6 +21,39 @@ public class Snake
     public void move(Cell nextCell){
         System.out.println("Snake is moving to "+nextCell.getRow()+ " "+nextCell.getCol());
 
+        Cell tail = snakePartList.removeLast();
+        tail.setCellType(CellType.EMPTY);
 
+        head = nextCell;
+        head.setCellType(CellType.SNAKE_NODE);
+        snakePartList.addFirst(head);
+    }
+
+    public boolean checkCrash(Cell nextCell){
+        System.out.println("Going to check for crash");
+        for(Cell cell : snakePartList){
+            if(cell == nextCell) return true;
+        }
+        return false;
+    }
+
+    public LinkedList<Cell> getSnakePartList ()
+    {
+        return snakePartList;
+    }
+
+    public void setSnakePartList (LinkedList<Cell> snakePartList)
+    {
+        this.snakePartList = snakePartList;
+    }
+
+    public Cell getHead ()
+    {
+        return head;
+    }
+
+    public void setHead (Cell head)
+    {
+        this.head = head;
     }
 }
